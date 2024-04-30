d15d7d3ff74bdebc830fceeb8e1bdb45d125764c
Bread <77288737+riley0122@users.noreply.github.com>
2024-04-30 18:27:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Progam
+{
+    private static DiscordSocketClient _client;
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    public static async Task Main()
+    {
+        _client = new();
+        _client.Log += Log;
+
+        var token = Environment.GetEnvironmentVariable("TOKEN");
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
